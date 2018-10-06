8b4f9ab23634354756a4c451d67943d52868a6df
DaiDV <dangvandai1992@gmail.com>
2018-10-06 18:50:20 +0700

    refs dev #1 create first app R2

---
+++ app/Http/Requests/CatRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Furbook\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CatRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:255',
+            'date_of_birth' => 'required|date_format:"Y-m-d"',
+            'breed_id' => 'required|numeric',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'required'    => 'Trường :attribute là bắt buộc',
+            'max'         => 'Trường :attribute tối đa là :max kí tự',
+            'numeric'     => 'Trường bắt buộc :attribute là kiểu số nguyên',
+            'date_format' => 'Trường :attribute format không đúng định dạng :format',
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+use Furbook\Http\Requests\CatRequest;
+
 Route::get('/', function () {
     /**
      * Test get query log
@@ -59,11 +61,20 @@ Route::get('/cats/create', function () {
 
 // Insert new cat
 Route::post('/cats', function () {
-    $validator = Validator::make(request()->all(), [
-        'name' => 'required|max:255',
-        'date_of_birth' => 'required|date_format:"Y-m-d"',
-        'breed_id' => 'required|numeric',
-    ]);
+    $validator = Validator::make(
+        request()->all(),
+        [
+            'name'          => 'required|max:255',
+            'date_of_birth' => 'required|date_format:"Y-m-d"',
+            'breed_id'      => 'required|numeric',
+        ],
+        [
+            'required'    => 'Trường :attribute là bắt buộc',
+            'max'         => 'Trường :attribute tối đa là :max kí tự',
+            'numeric'     => 'Trường bắt buộc :attribute là kiểu số nguyên',
+            'date_format' => 'Trường :attribute format không đúng định dạng :format',
+        ]
+    );
 
     if ($validator->fails()) {
         return redirect()
@@ -81,17 +92,27 @@ Route::post('/cats', function () {
 
 // Show form edit cat
 Route::get('/cats/{id}/edit', function ($id) {
-    return 'Show form edit cat #' . $id;
+    $breeds = Furbook\Breed::pluck('name', 'id');
+    $cat = Furbook\Cat::find($id);
+    //dd($cat);
+    return view('cats.edit', compact('cat', 'breeds'));
 })->name('cat.edit');
 
 // Update cat
-Route::put('/cats', function () {
-    return 'Update cat';
+Route::put('/cats/{id}', function (CatRequest $request, $id) {
+    $data = $request->all();
+    $cat = Furbook\Cat::find($id);
+    $cat->update($data);
+    return redirect()
+    ->route('cat.index');
 })->name('cat.update');
 
 // Delete cat
 Route::delete('/cats/{id}', function ($id) {
-    return 'Delete cat #' . $id;
+    $cat = Furbook\Cat::find($id);
+    $cat->delete();
+    return redirect()
+    ->route('cat.index');
 })->name('cat.destroy');
 
 // Test
